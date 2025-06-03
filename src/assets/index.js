2e6c1ca07a746d6b63f38bfa934aa046fad1775a
Luiza Ribeiro <84139409+luizaribeiro06@users.noreply.github.com>
2025-06-03 16:09:03 -0300

    Add files via upload

---
+++ src/assets/index.js
@@ -0,0 +1,34 @@
+
+import logo from "./logo-gs.png";
+import close from "./close.png";
+import menu from "./menu.png";
+import arrowUp from "./arrow-up.svg";
+import bombeiros from "./bombeiros-logo.png";
+import defesaCivil from "./defesa-civil-logo.png";
+import fiap from "./fiap-logo.svg";
+import ibm from "./ibm-logo.png";
+import inpe from "./inpe-logo.png";
+import medicos from "./medicos-sem-fronteiras-logo.png";
+import learning from "./learning-img.png";
+import mockup from "./mockup.png";
+import iot from "./iot.png";
+import apisgraficos from "./apis-graficos.jpeg";
+import iapreditiva from "./iapreditiva.jpeg";
+
+export {
+  close,
+  arrowUp,
+  logo,
+  menu,
+  bombeiros,
+  defesaCivil,
+  fiap,
+  ibm,
+  inpe,
+  medicos,
+  learning, 
+  mockup,
+  iapreditiva,
+  apisgraficos,
+  iot
+};
