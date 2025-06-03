cf5ea41af02a1e573dd8d6ed9ef56e0783f13365
Gabrielly Candido <gabriellycasilva@gmail.com>
2025-06-03 16:07:57 -0300

    Add files via upload

---
+++ tailwind.config.cjs
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  mode: "jit",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#2563EB",
+        primaryLight: "#3B82F6",
+        bgLight: "#EFF6FF",
+        offWhite: "#FDFDFD",
+        textDark: "#010613",
+        white: "#FFFFFF",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
+  },
+  plugins: [],
+};
